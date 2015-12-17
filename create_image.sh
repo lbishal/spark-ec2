89e060147bdbe21fec3ff829a83d504e5c100577
lbishal <lbishal@users.noreply.github.com>
2015-12-17 15:30:36 +0100

    Update create_image.sh

--- create_image.sh
+++ create_image.sh
@@ -29,7 +29,7 @@ sudo yum install -y ganglia ganglia-web ganglia-gmond ganglia-gmetad
 
 #test: Anaconda installation
 #download the installer
-wget https://3230d63b5fc54e62148e-c95ac804525aac4b6dba79b00b39d1d3.ssl.cf1.rackcdn.com/Anaconda2-2.4.1-Linux-x86_64.sh
+wget https://repo.continuum.io/archive/Anaconda2-2.4.1-Linux-x86_64.sh
 bash Anaconda2-2.4.1-Linux-x86_64.sh -b -p /root/Anaconda
 export PATH="/root/Anaconda/bin:$PATH"
 
